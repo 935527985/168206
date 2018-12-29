a32b50140f977bdd7869ed0264011af44bf0c7a7
935527985 <44491676+935527985@users.noreply.github.com>
2018-12-29 18:50:47 +0800

    Create 002.py

---
+++ 168206136/002.py
@@ -0,0 +1,20 @@
+def 判断水仙花数(num):
+        '判断一个数是否是水仙花数'
+        num = int(num)
+        if num < 100 or num > +1000:
+            print("不是水仙花数")
+        else:
+            个位 = num % 10
+            百位 = int(num / 100)
+            十位 = int((num - 百位 * 100) / 10)
+            # print(个位)
+            # print(十位)
+            # print(百位)
+            sum = 个位 * 个位 * 个位 + 十位 * 十位 * 十位 + 百位 * 百位 * 百位
+            if sum == num:
+                print("%d是水仙花数" % num)
+            else:
+                print("不是水仙花数")
+
+num = input()
+判断水仙花数(num)
